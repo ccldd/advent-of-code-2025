e51f46a897f7dcf7b6e43806b829612370439ee3
ccldd <cheboncruz@gmail.com>
2025-12-24 23:41:37 +1300

    finally parse 12

---
+++ 12/1.py
@@ -0,0 +1,38 @@
+import sys
+from dataclasses import dataclass
+from typing import Dict, List
+from io import StringIO
+
+f = open(sys.argv[1])
+
+
+@dataclass
+class Region:
+    width: int
+    length: int
+    quantities: List[int]
+
+
+shapes: Dict[int, List[str]] = {}
+regions: List[Region] = []
+
+lines = StringIO(f.read())
+while True:
+    line = lines.readline()
+    if line == "":
+        break
+
+    if "x" in line:
+        s = line.split(":")
+        width = int(s[0].split("x")[0])
+        length = int(s[0].split("x")[1])
+        quantities = [int(x) for x in s[1].split(" ") if x]
+        region = Region(width, length, quantities)
+        regions.append(region)
+    elif ":" in line:
+        num = line.split(":")[0]
+        shapes[int(num)] = []
+    elif line != "\n":
+        last_key = list(shapes.keys())[-1]
+        shapes[last_key].append(line)
+
